4267dc3f16fda9b896b30cc524f1f70393fe2fe6
Gage McGurie <72943845+Gage-McGuire@users.noreply.github.com>
2024-02-22 23:54:54 -0600

    Login page layout

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, Pressable } from 'react-native';
 import { useFonts } from 'expo-font';
 
 function Login(){
@@ -18,6 +18,42 @@ function Login(){
                     <Text style={styles.welcomeText}>Welcome</Text>
                     <Text style={styles.backText}>Back...</Text>  
                 </View>
+                <View style={styles.textContainer}>
+                    <Text style={styles.text}>What journey are we embarking on today?</Text>
+                </View>
+
+                <View style={styles.loginContainer}>
+                    <TextInput 
+                        style={styles.usernameInput}
+                        keyboardAppearance='dark'
+                        placeholder='username'
+                        placeholderTextColor='white'
+                        textAlign='left'
+                        textContentType='username'
+                    />
+                    <TextInput 
+                        style={styles.passwordInput}
+                        keyboardAppearance='dark'
+                        placeholder='password'
+                        placeholderTextColor='white'
+                        textAlign='left'
+                        textContentType='password'
+                        secureTextEntry='True'
+                    />
+                </View>
+
+                <View style={styles.loginContainer}>
+                    <Pressable>
+                        <Text style={styles.loginButton}>login</Text>
+                    </Pressable>
+                </View>
+
+                <View style={styles.loginContainer}>
+                    <Pressable>
+                        <Text style={styles.createAccountButton}>create account</Text>
+                    </Pressable>
+                </View>
+
             </SafeAreaView>
         </View>
     );
@@ -52,5 +88,75 @@ const styles = StyleSheet.create({
         textAlignVertical: 'auto',
         color: 'white',
         fontFamily: 'Koulen' 
+    },
+
+    text: {
+        position:'relative',
+        paddingLeft: 10,
+        fontSize: 20,
+        fontFamily: 'Koulen',
+        color: 'white',
+    },
+
+    textContainer: {
+        top: 200,
+    },
+
+    usernameInput: {
+        fontFamily: 'Koulen',
+        fontSize: 20,
+        color: 'white',
+        letterSpacing: 5,
+        borderColor: 'white',
+        borderWidth: 2,
+        borderRightWidth: 7,
+        borderBottomWidth: 7,
+        borderTopRightRadius: 10,
+        borderBottomLeftRadius: 10,
+        borderTopLeftRadius: 5,
+        height: 50,
+        width: 300,
+        alignContent: 'center',
+        paddingLeft: 10,
+    },
+
+    passwordInput: {
+        fontFamily: 'Koulen',
+        fontSize: 20,
+        color: 'white',
+        letterSpacing: 5,
+        borderColor: 'white',
+        borderWidth: 2,
+        borderRightWidth: 7,
+        borderBottomWidth: 7,
+        borderTopRightRadius: 10,
+        borderBottomLeftRadius: 10,
+        borderTopLeftRadius: 5,
+        height: 50,
+        width: 300,
+        alignContent: 'center',
+        marginTop: 30,
+        paddingLeft: 10,
+    },
+
+    loginContainer: {
+        top: 340,
+        alignItems:'center',
+    },
+
+    loginButton: {
+        fontFamily: 'Koulen',
+        letterSpacing: 3,
+        fontSize: 40,
+        color: 'white',
+        marginTop: 50,
+
+    },
+
+    createAccountButton: {
+        fontFamily: 'Koulen',
+        fontSize: 20,
+        color: 'white',
+        marginTop: 150,
     }
   });
